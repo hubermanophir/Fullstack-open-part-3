dcff129f5a6e127e3e576ca9f75bd34b0e705a88
hubermanophir <77234895+hubermanophir@users.noreply.github.com>
2021-03-07 21:36:24 +0200

    .

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ const express = require("express");
 const morgan = require("morgan");
 const app = express();
 const cors = require("cors");
+app.use(express.static("build"));
 app.use(cors());
 app.use(express.json());
 morgan.token("request-body", function (req, res) {
---
+++ front/build/precache-manifest.4c5155839bba7ff23c85b86a6fe06cec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04028b26d1c2a20acf8d48f596449740",
+    "url": "/index.html"
+  },
+  {
+    "revision": "183433b6789fad31a7c5",
+    "url": "/static/css/main.adb0e0ae.chunk.css"
+  },
+  {
+    "revision": "1224e9f809dff3239121",
+    "url": "/static/js/2.870ca22c.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.870ca22c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "183433b6789fad31a7c5",
+    "url": "/static/js/main.1c4ebdfa.chunk.js"
+  },
+  {
+    "revision": "b6b7328ed845ec8d67bd",
+    "url": "/static/js/runtime-main.e8880884.js"
+  }
+]);
